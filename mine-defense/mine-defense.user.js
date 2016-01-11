41410b3f4feb80aaafc965246bad9431c6083ff1
Glenn Moss <gim@keynetics.com>
2016-01-11 16:18:17 -0700

    Userscript for Mine Defense.

---
+++ mine-defense/mine-defense.user.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         Mine Defence
+// @namespace    https://github.com/glennimoss/incremental-toolkit/mine-defense
+// @version      0.01
+// @author       gim
+// @description  Automation for http://scholtek.com/minedefense
+// @match        http://scholtek.com/minedefense
+// @grant        none
+// @require      https://raw.githubusercontent.com/glennimoss/incremental-toolkit/master/lib/nums.js
+// ==/UserScript==
+/* jshint -W097 */
+var gim = {};
+(function () {
+'use strict';
+
+console.log("Installing Mine Defense automation...");
+var clicker = setInterval(function () {
+  MD.dig(250, 250);
+
+  for (var i=0; i < MD.MAGE_AURA_LEVEL.length; i++) {
+    if (MD.MAGE_AURA_LEVEL[i] < MD.MAX_MAGE_AURA) {
+      console.log("Clicking mage", i);
+      MD.clickMage(i);
+    }
+  }
+}, 200);
+
+var buyer = setInterval(function () {
+  var old_force_control = MD.FORCE_CONTROL;
+  MD.FORCE_CONTROL = true;
+  while (true) {
+    var sandshrews_owned = parse_num(_hired_re.exec($("#hireling3").text())[1])
+      , sandshrews_alch_rate = parse_num(_sandshrew_alch_rate_re.exec($("#alch-row-counter16").text())[1])
+      ;
+    if (sandshrews_owned > sandshrews_alch_rate) {
+      break;
+    }
+    MD.hire(3);
+  }
+  MD.FORCE_CONTROL = old_force_control;
+}, 500);
+
+gim.clear = function () {
+  clearInterval(clicker);
+  clearInterval(buyer);
+}
+
+})();
