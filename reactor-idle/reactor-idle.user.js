acb699a2e9d22b1593f179f85871114378efabf3
Glenn Moss <gim@keynetics.com>
2016-01-11 15:29:02 -0700

    Wrapped script in function so 'use strict' will apply properly.

--- reactor-idle/reactor-idle.user.js
+++ reactor-idle/reactor-idle.user.js
@@ -8,62 +8,64 @@
 // @require      https://raw.githubusercontent.com/glennimoss/incremental-toolkit/master/lib/nums.js
 // ==/UserScript==
 /* jshint -W097 */
-'use strict';
+(function () {
+  'use strict';
 
-console.log("Installing Reactor Idle automation...");
+  console.log("Installing Reactor Idle automation...");
 
-// Excessively complicated method to get a handle of the global game object.
-window.define("base/ConfirmedTimestamp", function () {
-  var t = 0;
-  return {
-    getConfirmedTimestamp: function () { return t; },
-    load: function (e) {
-      require([ "Main", "base/server/Kongregate", "base/server/Paypal", "base/server/Dummy", "base/ConfirmedTimestamp"
-              , "base/GoogleAdds", "base/UrlHandler", "logger", "numberFormat", "config/Event/GameEvent"
-              , "config/Event/ReactorEvent", "config/Event/UiEvent" ],
-        function (e, t, a, i, r, n, s) {
-          var cb = function () {
-            console.log("Initalizing game...");
-            var i = new e
-              , r = null
-              , o = s.identifySite();
-            r = "kongregate" == o ? new t(i) : new a(i);
-            i.setExternalApi(r);
-            i.init(!0);
-            window.gameobj = i
-            setTimeout(function () {
-              "direct" == o && n.init()
-            }, 1e3);
+  // Excessively complicated method to get a handle of the global game object.
+  window.define("base/ConfirmedTimestamp", function () {
+    var t = 0;
+    return {
+      getConfirmedTimestamp: function () { return t; },
+      load: function (e) {
+        require([ "Main", "base/server/Kongregate", "base/server/Paypal", "base/server/Dummy", "base/ConfirmedTimestamp"
+                , "base/GoogleAdds", "base/UrlHandler", "logger", "numberFormat", "config/Event/GameEvent"
+                , "config/Event/ReactorEvent", "config/Event/UiEvent" ],
+          function (e, t, a, i, r, n, s) {
+            var cb = function () {
+              console.log("Initalizing game...");
+              var i = new e
+                , r = null
+                , o = s.identifySite();
+              r = "kongregate" == o ? new t(i) : new a(i);
+              i.setExternalApi(r);
+              i.init(!0);
+              window.gameobj = i
+              setTimeout(function () {
+                "direct" == o && n.init()
+              }, 1e3);
+            }
+            console.log("Fetching timestamp...");
+            $.get("http://api.reactoridle.com/getTimestamp", function (n) {
+              isNaN(Number(n)) || (t = Number(n));
+              cb();
+            }).fail(function () {
+              cb();
+            })
           }
-          console.log("Fetching timestamp...");
-          $.get("http://api.reactoridle.com/getTimestamp", function (n) {
-            isNaN(Number(n)) || (t = Number(n));
-            cb();
-          }).fail(function () {
-            cb();
-          })
-        }
-      );
-    }
-  }
-});
-
-var seller;
-
-console.log("Requiring something from the game...");
-require(['game/actions/SellPowerManuallyAction'],
-  function (spm) {
-    console.log("Got our required object:", spm);
-    seller = setInterval(function () {
-      for (let name in gameobj.game.reactors) {
-        let r = gameobj.game.reactors[name]
-          , filled = r.getPower() / r.getMaxPower()
-          ;
-        if (filled > 0.9) {
-          console.log("Reactor", name, "at", (filled*100).toFixed(1) + "%, selling...");
-          (new spm(gameobj.game.reactors.metropolis)).sell();
-        }
+        );
       }
-    }, 5000);
-  }
-);
+    }
+  });
+
+  var seller;
+
+  console.log("Requiring something from the game...");
+  require(['game/actions/SellPowerManuallyAction'],
+    function (spm) {
+      console.log("Got our required object:", spm);
+      seller = setInterval(function () {
+        for (let name in gameobj.game.reactors) {
+          let r = gameobj.game.reactors[name]
+            , filled = r.getPower() / r.getMaxPower()
+            ;
+          if (filled > 0.9) {
+            console.log("Reactor", name, "at", (filled*100).toFixed(1) + "%, selling...");
+            (new spm(gameobj.game.reactors.metropolis)).sell();
+          }
+        }
+      }, 5000);
+    }
+  );
+})();
